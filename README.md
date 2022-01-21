82646362b5bf13534e9c3a6236f74b63690d47a3
Allan Almazan <aalmazan@users.noreply.github.com>
2022-01-21 09:44:49 -0800

    More examples (#3)
    * More examples
    
    * Add badge
    
    * Add `using` too

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # dotnet-anvil
-Anvil API library for .NET
+
+[Anvil](https://useanvil.com/) API library for .NET.
+
+![Nuget (with prereleases)](https://img.shields.io/nuget/vpre/Anvil)
+
+## Usage
+
+### Create a client instance
+
+```cs
+    using Anvil.Client;
+
+    var apiKey = "API-KEY-FROM-SETTINGS";
+    
+    // For GraphQL-related queries
+    var client = new GraphQLClient(apiKey);
+    
+    // If you are looking to use the PDF fill or PDF generate endpoints
+    // use the REST Client.
+    var restClient = new RestClient(apiKey);
+```
+
+### Example usage
+
+Also so the example in the `examples/` directory.
+
+```cs
+using Anvil.Client;
+using Anvil.Payloads.Request.Types;
+using Anvil.Payloads.Response;
+
+class Program
+{
+    const string apiKey = "MY-API-KEY";
+    
+    static async Task<bool> GeneratePdfExampleData()
+    {
+        var restClient = new RestClient(apiKey);
+
+        var payload = new Anvil.Payloads.Request.GeneratePdf
+        {
+            Title = "My Document Title",
+            Type = "markdown",
+            Data = new List<IGeneratePdfListable>
+            {
+                new GeneratePdfItem
+                {
+                    Label = "new thing1",
+                    Content = "Content1",
+                    FontSize = "20",
+                    TextColor = "#CC0000"
+                },
+                new GeneratePdfItem
+                {
+                    Label = "new thing2",
+                    Content = "Content2"
+                },
+            }
+        };
+
+        return await restClient.GeneratePdf(payload, "/path/for/file/what.pdf");
+    }
+}
+```
---
+++ examples/Example.cs
@@ -0,0 +1,251 @@
+using Anvil.Client;
+using Anvil.Payloads.Request.Types;
+using Anvil.Payloads.Response;
+
+namespace AnvilExample
+{
+    public class Example
+    {
+        const string apiKey = "my-api-key";
+
+        static async Task Main(string[] args)
+        {
+            var client = new GraphQLClient(apiKey);
+            var restClient = new RestClient(apiKey);
+
+            await CreateEtchPacketExistingTemplateExample();
+        }
+
+        static async Task GraphqlQueryExample()
+        {
+            var client = new GraphQLClient(apiKey);
+
+            var query = @"
+                query currentUser { currentUser { id } } 
+            ";
+
+            // `response` will be a `JObject` type and you can get different
+            // object fields by using the indexer operator (`[]`). Note that
+            // the field names are in the same case as in the query (camelCase)
+            // and won't follow the typical C# PascalCase as in the defined
+            // types in this library.
+            var response = await client.SendQuery(query, null);
+        }
+
+        static async Task GetCurrentUserExample()
+        {
+            var client = new GraphQLClient(apiKey);
+
+            // get the JSON property you want.
+            var response = await client.GetCurrentUser();
+
+            // Some things are JObject instances. You can use the indexer
+            // operator (`[]`) to get data within that object.
+            var userId = response.CurrentUser["id"];
+        }
+
+        static async Task GenerateEtchSignUrlExample()
+        {
+            var client = new GraphQLClient(apiKey);
+            var signerEid = "L41py2aJ2mQb40xXmI48   a";
+            var clientUserId = "your-systems-signer-id";
+            var data = await client.GenerateEtchSignUrl(signerEid, clientUserId);
+
+            var url = data.GenerateEtchSignUrl;
+        }
+
+        static async Task CreateEtchPacketNewPdfExample()
+        {
+            var client = new GraphQLClient(apiKey);
+
+            var path = "path/to/your/file.pdf";
+
+            // Convert the file bytes in into a base64 encoded string.
+            // We will use this directly in the payload.
+            var fileBytes = File.ReadAllBytes(path);
+            var b64File = Convert.ToBase64String(fileBytes);
+
+            // Prepare the file object in the payload
+            var payloadFile = new DocumentUpload
+            {
+                Id = "new-uploaded-file",
+                Title = "Please sign this",
+                File = new Base64FileUpload
+                {
+                    Data = b64File,
+                    Filename = "uploaded.pdf"
+                },
+                Fields = new List<SignatureField>
+                {
+                    new()
+                    {
+                        // Remember this `Id`. You will assign it to the signer below.
+                        Id = "signField1",
+                        Type = "signature",
+                        PageNum = 0,
+                        Rect = new Rect
+                        {
+                            X = 100,
+                            Y = 100,
+                            Width = 100,
+                            Height = 100,
+                        }
+                    }
+                }
+            };
+
+            var files = new List<IEtchPacketAttachable> {payloadFile};
+
+            // Create your signer and also assign it to a signature field 
+            // from the above template (the `Fields` property).
+            var signer = new EtchSigner
+            {
+                Id = "custom-signer-id",
+                Name = "Morgan Johnson",
+                Email = "morgan@example.com",
+                Fields = new[]
+                {
+                    new SignerField
+                    {
+                        // This field id is from the file payload above.
+                        FieldId = "signField1",
+                        FileId = "new-uploaded-file",
+                    }
+                }
+            };
+            var signers = new List<EtchSigner> {signer};
+
+            // Put it all together.
+            var payload = new Anvil.Payloads.Request.CreateEtchPacket
+            {
+                Name = "Etch packet new file",
+                IsDraft = false,
+                IsTest = true,
+                SignatureEmailBody = "Custom email body text",
+                SignatureEmailSubject = "Custom email subject",
+                Signers = signers.ToArray(),
+                Files = files.ToArray(),
+            };
+
+            var response = await client.CreateEtchPacket(payload);
+        }
+
+        static async Task CreateEtchPacketExistingTemplateExample()
+        {
+            var client = new GraphQLClient(apiKey);
+
+            // This is from your "Document Templates" area in your Anvil account.
+            var templateId = "template-id-from-anvil";
+
+            // Add one or more files to a List for the upcoming payload.
+            var files = new List<IEtchPacketAttachable>
+            {
+                new EtchCastRef
+                {
+                    // Remember this `Id`. You will assign it to the signer below.
+                    Id = "existingCastReference",
+                    CastEid = templateId,
+                }
+            };
+
+            // Create your signer and also assign it to a signature field 
+            // from the above template (the `Fields` property).
+            var signer = new EtchSigner
+            {
+                Id = "custom-signer-id",
+                Name = "Morgan Johnson",
+                Email = "morgan@example.com",
+                Fields = new[]
+                {
+                    new SignerField
+                    {
+                        FieldId = "sign1",
+                        FileId = "existingCastReference",
+                    }
+                }
+            };
+            var signers = new List<EtchSigner> {signer};
+
+            // Put it all together.
+            var payload = new Anvil.Payloads.Request.CreateEtchPacket
+            {
+                Name = "Etch packet name",
+                IsDraft = false,
+                IsTest = true,
+                SignatureEmailBody = "Email body",
+                SignatureEmailSubject = "Email subbb",
+                Signers = signers.ToArray(),
+                Files = files.ToArray(),
+            };
+
+            var response = await client.CreateEtchPacket(payload);
+        }
+
+        static async Task<object> DownloadDocumentsExample()
+        {
+            var restClient = new RestClient(apiKey);
+            return await restClient.DownloadDocuments("document-group-eid-here");
+        }
+
+        static async Task<object> GetEtchPacketExample()
+        {
+            var client = new GraphQLClient(apiKey);
+            return await client.GetEtchPacket("etch-packet-eid-here");
+        }
+
+        static async Task<ForgeSubmitPayload> ForgeSubmitExample()
+        {
+            var client = new GraphQLClient(apiKey);
+            var request = new Anvil.Payloads.Request.ForgeSubmit
+            {
+                ForgeEid = "forge-eid-here",
+                Payload = new
+                {
+                    Name = new
+                    {
+                        FirstName = "Taylor",
+                        LastName = "Jones"
+                    },
+                    Email = "tjones@example.com"
+                }
+            };
+            return await client.ForgeSubmit(request);
+        }
+
+        static async Task<bool> GeneratePdfExampleHtml()
+        {
+            var restClient = new RestClient(apiKey);
+
+            var payload = new Anvil.Payloads.Request.GeneratePdf
+            {
+                Title = "My PDFs title",
+                Type = "html",
+                Data = new GeneratePdfHtml
+                {
+                    Html = @"<div id='one'>This is some text</div>",
+                    Css = @"#one { color: red; }"
+                }
+            };
+
+            return await restClient.GeneratePdf(payload, "what.pdf");
+        }
+
+        static async Task<bool> GeneratePdfExampleData()
+        {
+            var restClient = new RestClient(apiKey);
+
+            var payload = new Anvil.Payloads.Request.GeneratePdf
+            {
+                Title = "my new title",
+                Type = "markdown",
+                Data = new List<IGeneratePdfListable>
+                {
+                    new GeneratePdfItem {Label = "new thing1", Content = "Content1"},
+                    new GeneratePdfItem {Label = "new thing2", Content = "Content2"},
+                }
+            };
+
+            return await restClient.GeneratePdf(payload, "what.pdf");
+        }
+    }
+}
